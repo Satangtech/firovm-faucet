22129cd028065c0c472214ac8077b484a034f1cd
Ic3Sandy <monkey_king2@hotmail.com>
2022-09-29 12:17:11 +0700

    ✅ Add unit test

--- src/assets/assets.controller.spec.ts
+++ src/assets/assets.controller.spec.ts
@@ -1,18 +1,77 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssetsController } from './assets.controller';
+import { AssetsService } from './assets.service';
+import { CreateAssetDto } from './dto/create-asset.dto';
 
 describe('AssetsController', () => {
   let controller: AssetsController;
+  let service: AssetsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AssetsController],
+      providers: [
+        {
+          provide: AssetsService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([
+              {
+                name: 'asset1',
+                balance: 100,
+                address: 'address1',
+                symbol: 'symbol1',
+                logo: 'logo1',
+                decimal: 1e18,
+              },
+            ]),
+            create: jest
+              .fn()
+              .mockImplementation((createAssetDto: CreateAssetDto) =>
+                Promise.resolve({ _id: '1', ...createAssetDto }),
+              ),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AssetsController>(AssetsController);
+    controller = module.get(AssetsController);
+    service = module.get(AssetsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create()', () => {
+    it('should create a new asset', async () => {
+      const createAssetDto: CreateAssetDto = {
+        name: 'name1',
+        balance: 100,
+        address: 'address1',
+        symbol: 'symbol1',
+        logo: 'logo1',
+        decimal: 1e18,
+      };
+
+      expect(controller.create(createAssetDto)).resolves.toEqual({
+        _id: '1',
+        ...createAssetDto,
+      });
+    });
+  });
+
+  describe('findAll()', () => {
+    it('should get an array of assets', () => {
+      expect(controller.findAll()).resolves.toEqual([
+        {
+          name: 'asset1',
+          balance: 100,
+          address: 'address1',
+          symbol: 'symbol1',
+          logo: 'logo1',
+          decimal: 1e18,
+        },
+      ]);
+    });
   });
 });
--- src/assets/assets.service.spec.ts
+++ src/assets/assets.service.spec.ts
@@ -1,15 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
 import { AssetsService } from './assets.service';
+import { Asset } from './interfaces/asset.interface';
 
-describe('AssetsService', () => {
+const mockAsset = {
+  name: 'asset1',
+  balance: 100,
+  address: 'address1',
+  symbol: 'symbol1',
+  logo: 'logo1',
+  decimal: 1e18,
+};
+
+const assetsArray = [
+  {
+    name: 'asset1',
+    balance: 100,
+    address: 'address1',
+    symbol: 'symbol1',
+    logo: 'logo1',
+    decimal: 1e18,
+  },
+  {
+    name: 'asset2',
+    balance: 100,
+    address: 'address1',
+    symbol: 'symbol1',
+    logo: 'logo1',
+    decimal: 1e18,
+  },
+];
+
+describe('AssetService', () => {
   let service: AssetsService;
+  let model: Model<Asset>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssetsService],
+      providers: [
+        AssetsService,
+        {
+          provide: 'ASSET_MODEL',
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockAsset),
+            constructor: jest.fn().mockResolvedValue(mockAsset),
+            find: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AssetsService>(AssetsService);
+    service = module.get(AssetsService);
+    model = module.get<Model<Asset>>('ASSET_MODEL');
   });
 
   it('should be defined', () => {
