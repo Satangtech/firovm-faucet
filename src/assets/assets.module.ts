017eb329c8ca2f62247ecefaa594b5df683680d6
Ic3Sandy <monkey_king2@hotmail.com>
2022-09-30 17:03:09 +0700

    🚧 Update assets service

--- src/assets/assets.module.ts
+++ src/assets/assets.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { DatabaseModule } from '../database/database.module';
+import { FiroRpcModule } from '../firo-rpc/firo-rpc.module';
 import { AssetsController } from './assets.controller';
 import { assetsProviders } from './assets.providers';
 import { AssetsService } from './assets.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    FiroRpcModule,
+    CacheModule.register(),
+  ],
   controllers: [AssetsController],
   providers: [AssetsService, ...assetsProviders],
 })
--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -1,14 +1,25 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Cache } from 'cache-manager';
 import { Model } from 'mongoose';
+
+import { FiroRpcService } from '../firo-rpc/firo-rpc.service';
 import { CreateAssetDto } from './dto/create-asset.dto';
 import { UpdateAssetDto } from './dto/update-asset.dto';
 import { Asset } from './interfaces/asset.interface';
 
 @Injectable()
 export class AssetsService {
+  private logger: Logger;
+
   constructor(
     @Inject('ASSET_MODEL') private readonly assetModel: Model<Asset>,
-  ) {}
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private configService: ConfigService,
+    private readonly firoRpcService: FiroRpcService,
+  ) {
+    this.logger = new Logger(AssetsService.name);
+  }
 
   async create(createAssetDto: CreateAssetDto): Promise<Asset> {
     const createdAsset = this.assetModel.create(createAssetDto);
@@ -16,7 +27,35 @@ export class AssetsService {
   }
 
   async findAll(): Promise<Asset[]> {
-    return this.assetModel.find().exec();
+    const cacheAssets = await this.cacheManager.get('assets');
+    if (cacheAssets) {
+      this.logger.log('Assets found in cache');
+      return <Asset[]>cacheAssets;
+    }
+
+    const assets = await this.assetModel.find().exec();
+    const assetsUpdate = await Promise.all(
+      assets.map(async (asset) => {
+        let balance = 0;
+        if (asset.address === '') {
+          balance = await this.firoRpcService.getNativeBalance();
+        } else {
+          balance = await this.firoRpcService.getAssetBalance(asset.address);
+        }
+        return await this.assetModel.findOneAndUpdate(
+          { address: asset.address },
+          { balance },
+          { new: true },
+        );
+      }),
+    );
+    await this.cacheManager.set(
+      'assets',
+      assetsUpdate,
+      60 * this.configService.get<number>('CACHE_TIMES_MINUTE'),
+    );
+    this.logger.log('Assets saved in cache');
+    return assetsUpdate;
   }
 
   async findOne(id: string): Promise<Asset> {
