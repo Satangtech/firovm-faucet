0bcded98556336613cc8c87341fa8a90cc380e2c
Ic3Sandy <monkey_king2@hotmail.com>
2022-10-03 15:41:16 +0700

    ✅ Update test for requests

--- src/requests/requests.controller.spec.ts
+++ src/requests/requests.controller.spec.ts
@@ -1,12 +1,27 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RequestsController } from './requests.controller';
+import { RequestsService } from './requests.service';
 
 describe('RequestsController', () => {
   let controller: RequestsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       controllers: [RequestsController],
+      providers: [
+        {
+          provide: RequestsService,
+          useValue: {
+            requestAsset: jest.fn().mockResolvedValue([
+              {
+                tx: 'tx1',
+              },
+            ]),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<RequestsController>(RequestsController);
--- src/requests/requests.service.spec.ts
+++ src/requests/requests.service.spec.ts
@@ -1,12 +1,39 @@
+import { CacheModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { FiroRpcModule } from '../firo-rpc/firo-rpc.module';
 import { RequestsService } from './requests.service';
+
+const mockAsset = {
+  name: 'asset1',
+  balance: 100,
+  address: 'address1',
+  symbol: 'symbol1',
+  logo: 'logo1',
+  decimal: 1e18,
+};
 
 describe('RequestsService', () => {
   let service: RequestsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RequestsService],
+      imports: [ConfigModule, CacheModule.register(), FiroRpcModule],
+      providers: [
+        RequestsService,
+        {
+          provide: 'ASSET_MODEL',
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockAsset),
+            constructor: jest.fn().mockResolvedValue(mockAsset),
+            findOneAndUpdate: jest.fn(),
+            find: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<RequestsService>(RequestsService);
