33d1be37a771fe977cd6ced8e0eb7773839def02
Ic3Sandy <monkey_king2@hotmail.com>
2022-10-03 14:19:06 +0700

    🚧 Update service firo for transfer

--- src/firo-rpc/firo-rpc.service.ts
+++ src/firo-rpc/firo-rpc.service.ts
@@ -15,6 +15,7 @@ export class FiroRpcService {
   private client: Client;
   private address: string;
   private rpcUrl: string;
+  private faucetAmount: number;
 
   constructor(private configService: ConfigService) {
     this.rpcUrl = this.configService.get<string>('RPC_URL');
@@ -25,6 +26,7 @@ export class FiroRpcService {
     this.rpc = new RPCClient(this.rpcUrl);
     this.client = new Client(this.rpcUrl);
     this.address = this.account.address().toString();
+    this.faucetAmount = this.configService.get<number>('FAUCET_AMOUNT');
   }
 
   getNetWork() {
@@ -60,5 +62,36 @@ export class FiroRpcService {
       throw new HttpException(error.message, 500);
     }
     return result;
+  }
+
+  async tokenTransfer(
+    tokenAddress: string,
+    nativeAddress: string,
+    tokenDecimal: number,
+  ): Promise<string> {
+    const txId = await this.client.tokenTransfer(
+      this.account,
+      tokenAddress,
+      nativeAddress,
+      BigInt(this.faucetAmount * tokenDecimal),
+    );
+    return txId;
+  }
+
+  async nativeTransfer(
+    nativeAddress: string,
+    tokenDecimal: number,
+  ): Promise<string> {
+    const txId = await this.client.sendFrom(
+      this.account,
+      [
+        {
+          to: nativeAddress as string,
+          value: this.faucetAmount * tokenDecimal,
+        },
+      ],
+      { feePerKb: 1000 },
+    );
+    return txId;
   }
 }
